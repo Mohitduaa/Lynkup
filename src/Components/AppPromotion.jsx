5f3af8d9e8cae2da6c06b86afb8b4ad501daa76d
Mohit Dua <duamohit10@gmail.com>
2025-02-18 15:43:08 +0530

    fotter upgrade

--- src/Components/AppPromotion.jsx
+++ src/Components/AppPromotion.jsx
@@ -2,22 +2,19 @@ import React from "react";
 
 const AppPromotion = () => {
   return (
-    <div className=" py-20 px-6 lg:px-20 flex flex-col lg:flex-row items-center justify-between   ">
-      <div className="text-center lg:text-left max-w-lg lg:pr-10">
-        <h1 className="text-3xl lg:text-5xl  text-white font-poppins font-medium mb-4 ">
-          Book and manage <br /> brand collaborations{" "}
+    <div className=" py-20 px-6 lg:px-20 flex flex-col lg:flex-row items-center justify-evenly  ">
+      <div className="text-center lg:text-left  lg:pr-10">
+        <h1 className="text-3xl lg:text-5xl  text-white font-poppins font-bold mb-4 ">
+          Book and manage <br /> brand collaborations{" "} <br />
           <span className="text-[#653FCC]">with one simple app</span>
         </h1>
-        <p className="text-white font-Lexend font-extralight mb-6 text-base lg:text-lg">
-          100% free for creators. Empower yourself with the best tools to manage
-          brand deals seamlessly.
-        </p>
+        
 
         <div className="flex justify-center lg:justify-start gap-4">
-          <div href="#" className="bg-[#272530] text-white flex items-center py-3 px-2 rounded-lg hover:bg-gray-800 transition">
+          <div href="#" className=" text-white flex items-center py-3 px-2 rounded-lg hover:bg-gray-800 transition">
             <img src="images/Appstore IOS.png" alt="App Store" className="h-10 "/>
           </div>
-          <div  href="#" className="bg-[#272530] text-white flex items-center py-3 px-2 rounded-lg hover:bg-gray-800 transition">
+          <div  href="#" className=" text-white flex items-center py-3 px-2 rounded-lg hover:bg-gray-800 transition">
             <img src="images/playstore.png" alt="Google Play" className="h-10 w-full"/>
           </div>
         </div>
@@ -49,8 +46,11 @@ const AppPromotion = () => {
             alt="App Mockup 2"
             className="w-full h-[600px]"
           />
+          <div className="-mt-10">
+
           <img className=" -rotate-[10deg] -ml-2 mb-2" src="images/right (2).png" alt="" />
         <h1 className="absolute -ml-12 text-white font-poppins font-medium">100%free creators</h1>
+          </div>
         </div>
       </div>
     </div>
