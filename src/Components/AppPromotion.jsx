31f0089cd524c375c6582afb9e2bfcf2e3d0934b
Mohit Dua <duamohit10@gmail.com>
2025-04-02 15:43:12 +0530

    app promition

--- src/Components/AppPromotion.jsx
+++ src/Components/AppPromotion.jsx
@@ -100,7 +100,7 @@ const AppPromotion = () => {
             <img
               src="images/mobilenew1.webp"
               alt="App Mockup 2"
-              className="w-full max-w-xs md:max-w-md h-auto object-contain"
+              className="md:w-full md:h-[600px] object-contain"
               style={{
                 transform: 'translateZ(0)' // Force GPU acceleration
               }}
